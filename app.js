3ed56c8e47f94a374346ef459784477bd248529d
lilikwn <lilik1.wahyu7@gmail.com>
2022-10-14 19:38:25 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const PORT = 3005;
+
+
+app.get('/', (req, res)=>{
+  return res.status(200).json({
+    status:true,
+    message: 'ini percobaan deploy ke platform VERCEL'
+  });
+});
+
+app.use((req, res) => {
+  return res.status(400).json({
+    status: false,
+    message: 'apa anda tersesat'
+  })
+});
+
+app.listen(PORT, 'localhost')
